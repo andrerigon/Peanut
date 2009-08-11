8e84aa7d8069ac69ba7e9d96384454d26c299104
André Ribeiro Gonçalves <andre.rigon@gmail.com>
2009-08-11 16:35:03 -0300

    exit signal handler

--- init.py
+++ init.py
@@ -1,4 +1,6 @@
 import sys
+import signal
+import os
 from PyQt4.QtGui import QApplication
 from spring import  *
 from springpython.context import ApplicationContext
@@ -7,5 +9,13 @@ if __name__ == "__main__":
 	app = QApplication(sys.argv)
 	fact = ApplicationContext(ActionBasedApplicationContext())
 	fact.get_object('NetworkAccessManager').start()
+	app.setQuitOnLastWindowClosed(True)
+	def exitAll():
+		os.killpg(os.getpgrp(), signal.SIGINT)
+	def handler(sig, arg):
+		print "Saindooooooo"
+		app.exit()
+	signal.signal(signal.SIGINT,handler)
+	app.connect(app, SIGNAL("lastWindowClosed()"), exitAll)
 	sys.exit(app.exec_())
 
